f570c361e4699301b255a4406401814e0695d2db
Markus Paulsen <ga96yen@mytum.de>
2020-03-26 10:09:45 +0100

    Changed to RxJs

--- Exercise1/Main.py
+++ Exercise1/Main.py
@@ -1,3 +1,8 @@
+# <editor-fold desc="Import RX">
+from rx import just, from_list
+from rx.operators import map, to_list, filter, flat_map, first
+# </editor-fold>
+
 from typing import *
 from Exercise1.City import City
 from Exercise1.Graph import Graph
@@ -46,40 +51,91 @@ class Main:
         return
 
     def uniform_cost_search(self):
-        initial_city: City = self._graph.get_graph()[self._graph.get_initial_state()]
-        goal_city: City = self._graph.get_graph()[self._graph.get_goal_state()]
-        self._open = [(initial_city, 0, [])]
+        self._open = (
+            just(
+                self._graph.get_graph()
+            )
+            .pipe(map(
+                lambda graph: (graph[self._graph.get_initial_state()], 0, [])
+            ))
+            .pipe(to_list())
+            .run()
+        )
         while self._open:
-            open_tuple: Tuple[City, int, List[City]] = self._open.pop(0)
-            head: City = open_tuple[0]
-            current_cost: int = open_tuple[1]
-            predecessors: List[City] = open_tuple[2]
-            self._closed.append(head.get_name())
-            if head.get_name() == goal_city.get_name():
+            open_tuple: Tuple[City, int, List[City]] = (
+                just(self._open)
+                .pipe(map(
+                    lambda open_list: open_list.pop(0)
+                ))
+                .pipe(first())
+                .run()
+            )
+            self._closed.append(open_tuple[0].get_name())
+            found_goal_state: bool = (
+                just(self._graph)
+                .pipe(map(
+                    lambda graph_object: graph_object.get_graph()
+                ))
+                .pipe(map(
+                    lambda graph: graph[self._graph.get_goal_state()]
+                ))
+                .pipe(map(
+                    lambda goal_state: goal_state.get_name()
+                ))
+                .pipe(map(
+                    lambda goal_state_name: goal_state_name == open_tuple[0].get_name()
+                ))
+                .pipe(first())
+                .run()
+            )
+            if found_goal_state:
                 print("States visited = " + str((len(self._closed))))
                 print(
                     "Found path of length "
-                    + str((len(predecessors) + 1))
+                    + str((len(open_tuple[2]) + 1))
                     + " with total cost "
-                    + str(current_cost)
+                    + str(open_tuple[1])
                     + ":"
                 )
-                for predecessor in predecessors:
-                    print(predecessor.get_name() + " =>")
-                print(head.get_name())
+                (
+                    from_list(open_tuple[2])
+                    .subscribe(
+                        on_next=lambda predecessor: print(predecessor.get_name() + " =>")
+                    )
+                )
+                print(open_tuple[0].get_name())
                 return
             preliminary_open: List[Tuple[City, int, List[City]]] = []
-            for road in head.get_roads():
-                if road.get_to() not in self._closed:
-                    preliminary_open.append(
+            (
+                just(open_tuple[0])
+                .pipe(flat_map(
+                    lambda current_city: current_city.get_roads()
+                ))
+                .pipe(map(
+                    lambda road: (road.get_to(), road.get_cost())
+                ))
+                .pipe(filter(
+                    lambda road_info: road_info[0] not in self._closed
+                ))
+                .subscribe(
+                    on_next=lambda road_info: preliminary_open.append(
                         (
-                            self._graph.get_graph()[road.get_to()],
-                            current_cost + road.get_cost(),
-                            predecessors + [head]
+                            self._graph.get_graph()[road_info[0]],
+                            open_tuple[1] + road_info[1],
+                            open_tuple[2] + [open_tuple[0]]
                         )
                     )
+                )
+            )
             preliminary_open.sort(key=lambda x: x[1])
-            self._open = self._open + preliminary_open
+            self._open = (
+                just(self._open)
+                .pipe(map(
+                    lambda open_list: open_list + preliminary_open
+                ))
+                .pipe(first())
+                .run()
+            )
         print("No route found")
         return
 
