97c96bf42a522a284cc3921d9450b2b0bc8c1924
Jorge Pérez Ramos <jorgeperezramos99@gmail.com>
2020-04-15 12:05:38 +0200

    Improve separation of different statements

--- Excercise2/Clause.py
+++ Excercise2/Clause.py
@@ -4,12 +4,17 @@ class Clause:
         self._atomList: List[str] = []
         self._clausesList: List[str] = []
         self._instructionsList: List[str] = []
+        self._goalClause: str = ""
+    def get_goalClause(self) -> str:
+        return self._goalClause
+
+    def set_goalClause(self, goalClause: str):
+        self._goalClause = goalClause
     def get_atomList(self) -> List[str]:
         return self._atomList
 
     def set_instructionsList(self, instructionsList: List[str]):
         self._instructionsList = instructionsList
-
     def get_instructionsList(self) -> List[str]:
         return self._instructionsList
 
@@ -29,14 +34,27 @@ class Clause:
 
         if not path.__contains__("input"):
             for line in doc_lines:
+
                 if line.__contains__("#"):
                     continue
-                if line.__contains__("~"):#Is a clause
-                  self._clausesList.append(line.strip("\n"))
+
+                if line.__contains__(" "):#Is a clause
+                    if line.__contains__("~v"):
+                        print("CONTIENE DISJOINT")
+                        line.replace("~v","*")
+                        additionalClause: List[str] = (line.split("~v"))
+                        for l in additionalClause:
+                            self._clausesList.append(l.strip("\n *").lower())
+                    else:
+                        self._clausesList.append(line.strip("\n*").lower())
                 else:
-                    self._atomList.append(line.strip("\n"))
+                        self._atomList.append(line.strip("\n*").lower())
+
+            self.set_goalClause(self._clausesList[-1])
+            self._clausesList.remove(self.get_goalClause())
+
         else:
             for line in doc_lines:
                 if line.__contains__("#"):
                     continue
-                self._instructionsList.append(line.strip("\n"))
+                self._instructionsList.append((line.lower()).strip("\n *"))
--- Excercise2/Main.py
+++ Excercise2/Main.py
@@ -1,5 +1,22 @@
 from FERAICourse2020.Excercise2.Clause import *
 
+""""""""""""""""
+def plResolution()://Sketch
+    clause = cnfConvert()
+    new = None
+    while():
+        for clauses in selectClauses(clauses):
+            resolvents = plresolver(c1,c2)
+            if resolvents.contain("NILL"):
+                return true
+        if clauses.contein(new)
+            return false
+        clauses.append(new)
+
+"""""""""""
+
+
+
 if __name__ == '__main__':
 
     caso = Clause()
