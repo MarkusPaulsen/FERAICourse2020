b4db5a1f414a340ac157e0d85d0e645a8d4cfefe
Jorge Pérez Ramos <jorgeperezramos99@gmail.com>
2020-05-22 14:03:32 +0200

    Add Configuration reading

---
+++ Exercise3/Configuration.py
@@ -0,0 +1,45 @@
+from typing import *
+class Configuration:
+    def __init__(self, mode,model,max_depth,num_trees,feature_ratio,example_ratio):
+        self._mode: int = mode
+        self._model: int = model
+        self._max_depth: int = max_depth
+        self._num_trees: int = num_trees
+        self._feature_ratio: int = feature_ratio
+        self._example_ratio: int = example_ratio
+
+    def get_mode(self) -> int:
+        return self._mode
+
+    def set_mode(self, mode: int):
+        self._mode = mode
+
+    def get_model(self) -> int:
+        return self._model
+
+    def set_model(self, model: int):
+        self._model = model
+
+    def get_max_depth(self) -> int:
+        return self._max_depth
+
+    def set_max_depth(self, max_depth: int):
+        self._max_depth = max_depth
+
+    def get_num_trees(self) -> int:
+        return self._num_trees
+
+    def set_num_trees(self, num_trees: int):
+        self._num_trees = num_trees
+
+    def get_feature_ratio(self) -> int:
+        return self._feature_ratio
+
+    def set_feature_ratio(self, feature_ratio: int):
+        self._feature_ratio = feature_ratio
+
+    def get_example_ratio(self) -> int:
+        return self._example_ratio
+
+    def set_example_ratio(self, example_ratio: int):
+        self._example_ratio = example_ratio
--- Exercise3/trainingSet.py
+++ Exercise3/trainingSet.py
@@ -1,9 +1,11 @@
 from typing import *
 from FERAICourse2020.Exercise3.Entry import *
+from FERAICourse2020.Exercise3.Configuration import *
 class trainingSet:
     def __init__(self):
         self._headers: List[str] = []
         self._entries: List[Entry] = []
+        self.config = Configuration()
 
     def get_headers(self) -> List[str]:
         return self._headers
@@ -19,9 +21,9 @@ class trainingSet:
 
 
 
-    def read_file(self, path: str):
-        doc = open(path, encoding="utf8")
-        doc_lines: List[str] = doc.readlines()
+    def read_file(self, training_set_path: str, config_path: str):
+        doc_training_set = open(training_set_path, encoding="utf8")
+        doc_lines: List[str] = doc_training_set.readlines()
         header_line: List[str] = doc_lines[0].split(",")
         self.set_headers(header_line)
 
@@ -39,3 +41,30 @@ class trainingSet:
             for word in split_line:
                 if not (word == "yes" or word == "no"):
                     temp_entry.append_element(word)
+
+
+        doc_config = open(config_path, encoding="utf8")
+        doc_config_lines: List[str] = doc_config.readlines()
+        temp_config = Configuration()
+
+        for line in doc_config_lines:
+            split_line = line.split("=")
+
+            if(split_line[0]=="mode"):
+                self.config.set_mode(split_line[1].strip(".\n"))
+            elif(split_line[0]=="model"):
+                self.config.set_model(split_line[1].strip(".\n"))
+            elif (split_line[0] == "max_depth"):
+                self.config.set_max_depth(split_line[1].strip(".\n"))
+            elif (split_line[0] == "num_trees"):
+                self.config.set_num_trees(split_line[1].strip(".\n"))
+            elif (split_line[0] == "feature_ratio"):
+                self.config.set_feature_ratio(split_line[1].strip(".\n"))
+            elif (split_line[0] == "example_ratio"):
+                self.config.set_example_ratio(split_line[1].strip(".\n"))
+            else:
+                print("Error in reading Config file")
+
+
+        doc_config.close()
+        doc_training_set.close()
